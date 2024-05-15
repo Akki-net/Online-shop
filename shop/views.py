ffbddab89ff0ae4a57474033352e4044f0af47da
Akki-net <akashsharma.zak@gmail.com>
2024-05-15 16:29:00 +0530

    creating catalog templates

--- shop/views.py
+++ shop/views.py
@@ -8,7 +8,7 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, 
                                     slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list/xhtml',
+    return render(request, 'shop/product/list.xhtml',
                 {'category': category,
                 'categories': categories,
                 'products': products})
